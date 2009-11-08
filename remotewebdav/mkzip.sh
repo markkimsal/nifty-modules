6f937bb24c9183b0df1b5c6aae498676ec658ef4
Mark <mark2@mt2009.com>
2009-11-08 10:42:36 -0500

    Better zip filename for packaging

--- remotewebdav/mkzip.sh
+++ remotewebdav/mkzip.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-VER=`git rev-parse --short HEAD`
-PKG="Metrof_RemoteWebdav-1.0.$VER.zip"
-echo $VER
+VER=`grep "version" meta.ini`
+VER=`echo "$VER" | cut -c 16-`
+REV=`git rev-parse --short HEAD`
+PKG="Metrof_RemoteWebdav-1.$VER-$REV.zip"
+#echo $VER
 cd ..
 zip -r $PKG remotewebdav -x "remotewebdav/.git*" -x "*~" -x "remotewebdav/mkzip.sh"
